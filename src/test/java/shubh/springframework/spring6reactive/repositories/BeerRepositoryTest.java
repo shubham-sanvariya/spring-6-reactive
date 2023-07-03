a62b58d071eed90d89ce808a277ffb08c96888d9
shubham-sanvariya <shubhamsanvariyait.com>
2023-07-03 13:33:18 +0530

    adding BeerRepositoryTest creating and saving Beer

---
+++ src/test/java/shubh/springframework/spring6reactive/repositories/BeerRepositoryTest.java
@@ -0,0 +1,36 @@
+package shubh.springframework.spring6reactive.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import shubh.springframework.spring6reactive.domain.Beer;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataR2dbcTest
+class BeerRepositoryTest {
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void saveNewBeer() {
+        beerRepository.save(getTestBeer())
+                .subscribe(beer -> {
+                    System.out.println(beer.toString());
+                });
+    }
+
+    Beer getTestBeer() {
+        return Beer.builder()
+                .beerName("space out")
+                .beerStyle("IPA")
+                .price(BigDecimal.TEN)
+                .quantityOnHand(12)
+                .upc("12331233")
+                .build();
+    }
+
+}
