e6a180c96af1b6017dd718dcbcee32703a3ab1d0
shubham-sanvariya <shubhamsanvariyait.com>
2023-07-04 12:35:57 +0530

    adding getBeerById in BeerService

--- src/main/java/shubh/springframework/spring6reactive/service/BeerService.java
+++ src/main/java/shubh/springframework/spring6reactive/service/BeerService.java
@@ -1,8 +1,11 @@
 package shubh.springframework.spring6reactive.service;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import shubh.springframework.spring6reactive.model.BeerDTO;
 
 public interface BeerService {
     Flux<BeerDTO> listBeers();
+
+    Mono<BeerDTO> getBeerById(Integer beerId);
 }
