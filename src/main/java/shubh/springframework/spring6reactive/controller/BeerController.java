8492b6d0b8d7a7712132b76c2930691b24d29cd4
shubham-sanvariya <shubhamsanvariyait.com>
2023-07-04 19:34:29 +0530

    adding postMapping for saving beer

--- src/main/java/shubh/springframework/spring6reactive/controller/BeerController.java
+++ src/main/java/shubh/springframework/spring6reactive/controller/BeerController.java
@@ -1,9 +1,9 @@
 package shubh.springframework.spring6reactive.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import shubh.springframework.spring6reactive.model.BeerDTO;
@@ -16,6 +16,17 @@ public class BeerController {
     public static final String  BEER_PATH_ID = BEER_PATH + "/{beerId}";
 
     private final BeerService beerService;
+
+    @PostMapping(BEER_PATH)
+    Mono<ResponseEntity<Void>> createNewBeer(@RequestBody BeerDTO beerDTO){
+       return beerService.saveNewBeer(beerDTO)
+               .map(savedDto -> ResponseEntity.created(UriComponentsBuilder
+                       .fromHttpUrl("http://localhost:8080" + BEER_PATH
+                            + "/" +savedDto.getId())
+                       .build().toUri())
+                       .build());
+
+    }
 
     @GetMapping(BEER_PATH_ID)
     Mono<BeerDTO> getBeerById(@PathVariable("beerId") Integer beerId)
