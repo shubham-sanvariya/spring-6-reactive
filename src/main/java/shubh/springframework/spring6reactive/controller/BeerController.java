27d1dded11ae27a19fa535d0209b6aca0fd1df8e
shubham-sanvariya <shubhamsanvariyait.com>
2023-07-04 12:36:53 +0530

    mapping Beer_Path_id in controller

--- src/main/java/shubh/springframework/spring6reactive/controller/BeerController.java
+++ src/main/java/shubh/springframework/spring6reactive/controller/BeerController.java
@@ -2,8 +2,10 @@ package shubh.springframework.spring6reactive.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import shubh.springframework.spring6reactive.model.BeerDTO;
 import shubh.springframework.spring6reactive.service.BeerService;
 
@@ -11,8 +13,15 @@ import shubh.springframework.spring6reactive.service.BeerService;
 @RequiredArgsConstructor
 public class BeerController {
     public static final String BEER_PATH = "/api/v2/beer";
+    public static final String  BEER_PATH_ID = BEER_PATH + "/{beerId}";
 
     private final BeerService beerService;
+
+    @GetMapping(BEER_PATH_ID)
+    Mono<BeerDTO> getBeerById(@PathVariable("beerId") Integer beerId)
+    {
+        return beerService.getBeerById(beerId);
+    }
 
     @GetMapping(BEER_PATH)
     Flux<BeerDTO> listBeers() {
