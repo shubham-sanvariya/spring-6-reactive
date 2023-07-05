1ca12615c8d67a5e6c20e3656f82f9e5fd307987
shubham-sanvariya <shubhamsanvariyait.com>
2023-07-06 00:31:54 +0530

    changing status from ok to noContent

--- src/main/java/shubh/springframework/spring6reactive/controller/BeerController.java
+++ src/main/java/shubh/springframework/spring6reactive/controller/BeerController.java
@@ -37,7 +37,7 @@ public class BeerController {
     Mono<ResponseEntity<Void>> updateExistingBeer(@PathVariable("beerId") Integer beerId,
                                                  @Validated @RequestBody BeerDTO beerDTO){
         return beerService.updateBeer(beerId, beerDTO)
-                .map(savedDTO -> ResponseEntity.ok().build());
+                .map(savedDTO -> ResponseEntity.noContent().build());
     }
 
     @PostMapping(BEER_PATH)
