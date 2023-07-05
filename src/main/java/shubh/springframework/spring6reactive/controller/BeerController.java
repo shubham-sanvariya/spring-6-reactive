87147d78fb93744b3d284441181753c867257dcc
shubham-sanvariya <shubhamsanvariyait.com>
2023-07-06 00:40:26 +0530

    changing map to thenReturn in DeleteMethod

--- src/main/java/shubh/springframework/spring6reactive/controller/BeerController.java
+++ src/main/java/shubh/springframework/spring6reactive/controller/BeerController.java
@@ -20,7 +20,7 @@ public class BeerController {
 
     @DeleteMapping(BEER_PATH_ID)
     Mono<ResponseEntity<Void>> deleteById(@PathVariable Integer beerId){
-       return beerService.deleteBeerById(beerId).map(response -> ResponseEntity.noContent().build());
+       return beerService.deleteBeerById(beerId).thenReturn(ResponseEntity.noContent().build());
     }
 
     //The @ResponseBody annotation tells a controller that the object returned is automatically serialized into JSON and passed back into the HttpResponse object
