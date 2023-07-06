60a6b8b97bd17c45309aa90e049c989d37f88b1e
shubham-sanvariya <shubhamsanvariyait.com>
2023-07-06 19:23:05 +0530

    adding CustomerDTO

---
+++ src/main/java/shubh/springframework/spring6reactive/model/CustomerDTO.java
@@ -0,0 +1,19 @@
+package shubh.springframework.spring6reactive.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CustomerDTO {
+    private Integer id;
+    private String customerName;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+}
