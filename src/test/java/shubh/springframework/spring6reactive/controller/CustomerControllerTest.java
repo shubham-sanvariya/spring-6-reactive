be31282a21f2cabe2f9e61ca5ae76325be90b2f3
shubham-sanvariya <shubhamsanvariyait.com>
2023-07-07 14:29:32 +0530

    adding test for creating customer

--- src/test/java/shubh/springframework/spring6reactive/controller/CustomerControllerTest.java
+++ src/test/java/shubh/springframework/spring6reactive/controller/CustomerControllerTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
 import shubh.springframework.spring6reactive.model.CustomerDTO;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -32,6 +33,23 @@ class CustomerControllerTest {
                 .expectStatus().isOk()
                 .expectHeader().valueEquals("content-type", "application/json")
                 .expectBody(CustomerDTO.class);
+    }
+
+    @Test
+    void testCreateCustomer() {
+
+        webTestClient.post().uri(CustomerController.CUSTOMER_PATH)
+                .body(Mono.just(getCustomerDto()), CustomerDTO.class)
+                .header("Content-Type", "application/json")
+                .exchange()
+                .expectStatus().isCreated()
+                .expectHeader().location("http://localhost:8080/api/v2/customer/4");
+    }
+
+    public static CustomerDTO getCustomerDto(){
+        return CustomerDTO.builder()
+                .customerName("Test Customer")
+                .build();
     }
 
 }
