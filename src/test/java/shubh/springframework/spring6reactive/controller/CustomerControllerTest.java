02ae7309b7e49d161a300272d48cb22446ddf382
shubham-sanvariya <shubhamsanvariyait.com>
2023-07-07 13:56:41 +0530

    adding test for getCustomerById

--- src/test/java/shubh/springframework/spring6reactive/controller/CustomerControllerTest.java
+++ src/test/java/shubh/springframework/spring6reactive/controller/CustomerControllerTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import shubh.springframework.spring6reactive.model.CustomerDTO;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,6 +23,15 @@ class CustomerControllerTest {
                 .expectStatus().isOk()
                 .expectHeader().valueEquals("content-type", "application/json")
                 .expectBody().jsonPath("$.size()").isEqualTo(3);
+    }
+
+    @Test
+    void getCustomerById() {
+        webTestClient.get().uri(CustomerController.CUSTOMER_PATH_ID,1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().valueEquals("content-type", "application/json")
+                .expectBody(CustomerDTO.class);
     }
 
 }
