c461b9cbf938c9a2dfdb211b50649aaf077d2d7c
shubham-sanvariya <shubhamsanvariyait.com>
2023-07-07 13:52:01 +0530

    adding test for listingCustomers

---
+++ src/test/java/shubh/springframework/spring6reactive/controller/CustomerControllerTest.java
@@ -0,0 +1,27 @@
+package shubh.springframework.spring6reactive.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+class CustomerControllerTest {
+
+    @Autowired
+    WebTestClient   webTestClient;
+
+    @Test
+    void listCustomers() {
+        webTestClient.get().uri(CustomerController.CUSTOMER_PATH)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().valueEquals("content-type", "application/json")
+                .expectBody().jsonPath("$.size()").isEqualTo(3);
+    }
+
+}
